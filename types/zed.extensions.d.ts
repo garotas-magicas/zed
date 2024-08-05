c42e15bfb59749d3baea80de0f9f72db4d262960
nicaksks <juliesuzuya@outlook.com>
2024-08-05 13:13:20 -0300

    zed

---
+++ types/zed.extensions.d.ts
@@ -0,0 +1,31 @@
+function number(x: any): number;
+function string(x: any): string;
+
+interface String {
+    /* String */
+    string(): string;
+    slug(): string;
+
+    /* Number */
+    number(): number;
+}
+
+interface Number {
+    /* String */
+    string(): string;
+
+    /* Number */
+    number(n: number): number;
+    min(n: number): number;
+    max(n: number): number;
+}
+
+interface Object {
+    /* String */
+    string(): string;
+
+    /* Number */
+    number(): number;
+    min(n: number): number;
+    max(n: number): number;
+}
