4bc8cfb4c3432f46d3ad57e399a72ddd45024830
nicaksks <juliesuzuya@outlook.com>
2024-08-05 03:37:38 -0300

    add: paths

--- external.ts
+++ external.ts
@@ -1,8 +1,8 @@
 /* Methods */
-export * from './methods/string';
-export * from './methods/number';
-export * from './methods/object';
+export * from '@methods/string';
+export * from '@methods/number';
+export * from '@methods/object';
 
 /* Functions */
-export * from './functions/string';
-export * from './functions/number'
+export * from '@functions/string';
+export * from '@functions/number'
